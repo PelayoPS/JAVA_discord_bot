4f1677bfd1d75518d29f69f7dff0a1d2347c2be8
PelayoPS <pelayops1041@gmail.com>
2022-10-08 20:32:28 +0200

    new build

--- src/main/java/src/listeners/OnUserJoin.java
+++ src/main/java/src/listeners/OnUserJoin.java
@@ -1,5 +1,7 @@
 package src.listeners;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import src.DiscordBot;
@@ -18,5 +20,17 @@ public class OnUserJoin extends ListenerAdapter {
     @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent event) {
         new UserJoinLogger(bot).logEvent(event, DiscordBot.isProductionEnabled());
+        String channelString = DiscordBot.getConfig().get("SERVERJOINCHANNELID");
+        event.getGuild().getTextChannelById(channelString)
+                .sendMessageEmbeds(getWelcomeMessage(event)).queue();
+    }
+
+    private MessageEmbed getWelcomeMessage(GuildMemberJoinEvent event) {
+        MessageEmbed embed = new EmbedBuilder()
+                .setTitle("Welcome to the server!")
+                .setDescription("Welcome to the server! Please read the rules and enjoy your stay!")
+                .setAuthor(event.getUser().getAsMention(), null, event.getUser().getAvatarUrl() + "?format=png&dynamic=true&size=1024")
+                .build();
+        return embed;
     }
 }
